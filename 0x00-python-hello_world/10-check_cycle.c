d84b15ecef25e84d9b88e77e5ea600f7ccb3e9f1
sejo-moyez <moyajoseph439@gmail.com>
2022-12-12 08:43:11 +0300

    created

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,1 +1,21 @@
-i
+#include "lists.h"
+/**
+ *check_cycle - checks if a singly linked list has a cycle in it
+ *@list: a pionter to the sinly linked list to check
+ *
+ *Return: 1 there is a cycle or 0 if there in no cycle
+ *Description: check cycle for ALX project
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *fast = list, *slow = list;
+
+	while (fast && fast->next)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+		if (fast == slow)
+			return (1);
+	}
+	return (0);
+}
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,1 +1,20 @@
-i
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *struct listint_s - singly linked list
+ *@n: integer
+ *@next: points to the next node
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+int check_cycle(listint_t *list);
+
+#endif /* LISTS_H */
