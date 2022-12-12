040ff2823fbbbfb32591e70a781c94340d0a46b3
sejo-moyez <107212756+sejo-moyez@users.noreply.github.com>
2022-12-12 23:35:28 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
-ii
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+argc = len(argv)
+if argc == 1:
+    print("0 arguments.")
+elif argc == 2:
+    print("1 argument:")
+    print("1: {}".format(argv[1]))
+else:
+    print("{:d} arguments:".format(argc - 1))
+    for i in range(1, argc):
+        print("{:d}: {}".format(i, argv[i]))
