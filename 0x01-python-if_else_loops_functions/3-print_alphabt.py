6c5fb717987877d2d8b42c853f9e20ec0f6f9afb
sejo-moyez <107212756+sejo-moyez@users.noreply.github.com>
2022-12-12 18:09:14 +0300

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,1 +1,8 @@
-ii
+#!/usr/bin/python3
+num = 97
+while num < 123:
+    if num == 101 or num == 113:
+        num = num + 1
+        continue
+    print("{}".format(chr(num)), end="")
+    num = num + 1
