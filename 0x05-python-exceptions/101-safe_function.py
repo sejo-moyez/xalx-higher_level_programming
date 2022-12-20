e4a5c2b3c05ab24a95b6bdb85295a87e988fb6c9
sejo-moyez <107212756+sejo-moyez@users.noreply.github.com>
2022-12-20 05:58:15 +0300

    Update 101-safe_function.py

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-
 import sys
 
 
 def safe_function(fct, *args):
-    """Executes a function safely.
-    Args:
-        fct: The function to execute.
-        args: Arguments for fct.
-    Returns:
-        If an error occurs - None.
-        Otherwise - the result of the call to fct.
-    """
     try:
         result = fct(*args)
-        return (result)
-    except:
-        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
-        return (None)
+    except ZeroDivisionError:
+        result = None
+        sys.stderr.write("Exception: division by zero\n")
+    except IndexError:
+        result = None
+        sys.stderr.write("Exception: list index out of range\n")
+
+    return result
